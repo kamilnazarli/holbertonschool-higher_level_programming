b7e113356b9bfda1e317da8bfc15e6d8ecf9f53c
kamil <nezerovk425@gmail.com>
2025-11-28 21:33:01 +0000

    To make an input uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for i in (str + "\n"):
         if ord(i) >= 97:
-           print("{}".format(chr(ord(i) - 32)), end = "")
+            print("{}".format(chr(ord(i) - 32)), end="")
         else:
-           print("{}".format(i), end = "")
+            print("{}".format(i), end="")
