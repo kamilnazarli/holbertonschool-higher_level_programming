cef8f457fc3fb25dbc268f864dbab28b13ba5ba3
KAmil <nezerovk425@gmail.com>
2025-11-28 18:22:30 +0000

    To define fizzbuzz function

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 100):
         if i % 5 == 0 and i % 3 == 0:
-	    print("FizzBuzz", sep=" ")
-	elif i % 3 == 0:
-	    print("Fizz", sep=" ")
-	elif i % 5 == 0:
+            print("FizzBuzz", sep=" ")
+        elif i % 3 == 0:
+            print("Fizz", sep=" ")
+        elif i % 5 == 0:
             print("Buzz", sep=" ")
-	else:
-	    print(i, sep=" ")
+        else:
+            print(i, sep=" ")
