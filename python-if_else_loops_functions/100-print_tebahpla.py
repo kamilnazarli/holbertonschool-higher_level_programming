90e249d783ecabcc38f7e12eeb101b2dc31342f7
Kamil <nezerovk4252gmail.com>
2025-12-02 09:43:06 +0000

    To alternate letters

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-#to alternate lowercase and uppercase letters
+# to alternate lowercase and uppercase letters
 for i in range(90, 64, -1):
     print("{}".format(chr(i)) if i % 2 != 0 else chr(i + 32), end='')
