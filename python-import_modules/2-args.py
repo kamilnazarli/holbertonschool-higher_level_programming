f3eea1bba160a28813e9f6f90fe1586a9fc782c5
Kamil <nezerovk4252gmail.com>
2025-12-02 13:04:01 +0000

    To print arguments of argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("{} {}.".format(len(sys.argv)-1, "arguments"))
     elif len(sys.argv) == 2:
-       print("{} {}:".format(len(sys.argv)-1, "argument"))
-       print("{}: {}".format(len(sys.argv)-1, sys.argv[1]))
+        print("{} {}:".format(len(sys.argv)-1, "argument"))
+        print("{}: {}".format(len(sys.argv)-1, sys.argv[1]))
     else:
         print("{} {}:".format(len(sys.argv)-1, "arguments"))
         for i in range(1, len(sys.argv) + 1):
