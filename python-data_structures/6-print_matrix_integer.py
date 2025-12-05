3e5fdc7778e3f7487039636e78eeafe89724f6ae
Kamil <nezerovk425@gmail.com>
2025-12-05 17:26:26 +0000

    To print matrix integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[]):
-    for row in matrix:
-        for el in row:
-            print("{:d}".format(el), end = ' ')
-        print()
+    for row in range(len(matrix)):
+        for el in range(len(matrix[row])):
+            if el == len(matrix[row]) - 1:
+                print("{:d}".format(matrix[row][el]))
+            else:
+                print("{:d}".format(matrix[row][el]), end = " ")
