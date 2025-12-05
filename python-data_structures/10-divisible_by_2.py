98859c5541d31fbd76668b71b75bfc2c806c5b95
Kamil <nezerovk425@gmail.com>
2025-12-05 18:25:46 +0000

    To find integers divisible by 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    bool_=[]
+    for i in my_list:
+        if i % 2 == 0:
+            bool_.append(True)
+        else:
+            bool_.append(False)
+    return bool_
