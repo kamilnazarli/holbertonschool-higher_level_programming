2adca0d2844f038d8aeaf52bda0666e801ae4683
Kamil <nezerovk425@gmail.com>
2025-12-05 16:57:25 +0000

    To replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     temp = my_list[:]
     if idx >= len(my_list) or idx < 0:
         return temp
