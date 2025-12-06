1dbed5b25c5f08effec1f6f04ddca40fb9220d3f
Kamil <nezerovk425@gmail.com>
2025-12-06 13:56:22 +0000

    To print integers from list safely

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    sum, count = 0, 0
-    for i in range(x):
-        try:
-            sum = sum * 10 + my_list[i]
-            count += 1
-        except IndexError:
-            continue
-        except TypeError:
-            continue
-    print("{:d}".format(sum))
-    return count
+    if x != 0:
+        sum, count = 0, 0
+        for i in range(x):
+            try:
+                sum = sum * 10 + my_list[i]
+                count += 1
+            except IndexError:
+                continue
+            except TypeError:
+                continue
+        print("{:d}".format(sum))
+        return count
+    return 0
