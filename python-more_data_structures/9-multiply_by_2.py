8af524b834b9850ccc9a6dc901a0734e77451398
Kamil <nezerovk425@gmail.com>
2025-12-06 09:24:22 +0000

    To multiply each element by 2 in a dictionary

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = {}
     for i in list(a_dictionary):
-        a_dictionary.update({i: a_dictionary.get(i) * 2})
-    return a_dictionary
+        new_dict.update({i: a_dictionary.get(i) * 2})
+    return new_dict
