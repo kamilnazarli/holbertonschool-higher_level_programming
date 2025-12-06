7918d0a5d767ceca66cc88625072af67c414b3b8
Kamil <nezerovk425@gmail.com>
2025-12-06 11:28:57 +0000

    To print list safely

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    sum = 0
+    sum, count = 0, 0
     try:
         for i in range(x):
             sum = sum * 10 + my_list[i]
+            count += 1
     except IndexError:
-        sum = 0
+        sum, count = 0, 0
         for i in my_list:
             sum = sum * 10 + i
-    return sum
+            count += 1
+    return f"{sum}\n{count}"
