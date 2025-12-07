b970e3bad7e6afd4db668f674e5c1dcde51ebbb1
Kamil <nezerovk425@gmail.com>
2025-12-07 19:14:40 +0000

    To convert from roman to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str:
+    if isinstance(roman_string, str):
+        roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        sum = 0
+        for i in range(len(roman_string)):
+            if i == len(roman_string) - 1:
+                sum = sum + roman_dict.get(roman_string[i])
+            elif roman_dict.get(roman_string[i]) >= roman_dict.get(roman_string[i + 1]):
+                sum = sum + roman_dict.get(roman_string[i])
+            else:
+                sum = sum - roman_dict.get(roman_string[i])
+        return sum
+    else:
         return 0
-    roman_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    sum = 0
-    for i in range(len(roman_string)):
-        if i == len(roman_string) - 1:
-            sum = sum + roman_dict.get(roman_string[i])
-        elif roman_dict.get(roman_string[i]) >= roman_dict.get(roman_string[i + 1]):
-            sum = sum + roman_dict.get(roman_string[i])
-        else:
-            sum = sum - roman_dict.get(roman_string[i])
-    return sum
