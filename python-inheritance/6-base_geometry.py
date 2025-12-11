97047bb079fec02d8970022115e500de3eddc78f
Kamil <nezerovk425@gmail.com>
2025-12-11 19:41:22 +0000

    To raise an error inside a class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module creates a function to check instance
+"""
+
+
+class BaseGeometry:
+    """
+    A class for geometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
