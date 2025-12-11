cbaae7159f6cb2e0ad3933dbeeb774a3e1162552
Kamil <nezerovk425@gmail.com>
2025-12-11 11:34:55 +0000

    To print __str__

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+This module creates a Robot class
+"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle by its width and height:
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        temp = ""
+        for i in range(self.height):
+            for j in range(self.width):
+                temp += "#"
+            if i != self.height - 1:
+                temp += "\n"
+        return temp
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not (isinstance(value, int)):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width mus be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not (isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height mus be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
