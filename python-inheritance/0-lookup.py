05572dcb1ed7754dfc244b22a52389d272e101c9
Kamil <nezerovk425@gmail.com>
2025-12-11 18:21:52 +0000

    To look up all attributes

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+This module defines a function which shows all available attributes
+"""
+
+
 def lookup(cls):
+    """
+    This function returns all attributes inside a class
+    """
     return dir(cls)
