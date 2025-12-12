01ecea33b418c88d698df7656053f07eb734ce3e
Kamil <nezerovk425@gmail.com>
2025-12-12 17:09:37 +0000

    To override a function inside Rectangle class

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 This module shows overriding
 """
 
+
+BaseGeometry = __import__(7-base_geometry).BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
