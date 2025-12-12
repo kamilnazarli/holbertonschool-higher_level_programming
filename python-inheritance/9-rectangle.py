d348aae1d42476f53c61435f7038c7cc52f91887
Kamil <nezerovk425@gmail.com>
2025-12-12 17:05:35 +0000

    To override a function inside Rectangle class

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class Rectangle(BaseGeometry):
+    """
+    This class creates a rectangle with its width and height.
+    it inherits from base geometry class.
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        return self.__width * self.__height
