13b4028a4b51bbbb1b9eadc3643b601186bd64fb
Kamil <nezerovk425@gmail.com>
2025-12-12 16:40:43 +0000

    To create a rectangle subclass

---
+++ python-inheritance/tests/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module creates a function to check instance
+"""
+
+
+class BaseGeometry:
+    """
+    A class for geometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """
+    This class creates a rectangle with its width and height.
+    it inherits from base geometry class.
+    """
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator(width, width)
+        self.integer_validator(height, height)
