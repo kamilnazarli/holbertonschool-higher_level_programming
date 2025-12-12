5620c24f493751d7267ca2430a26db4b25fc1f0c
Kamil <nezerovk425@gmail.com>
2025-12-12 16:56:40 +0000

    To create a rectangle subclass

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,7 +4,7 @@ This module creates a function to check instance
 """
 
 
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
