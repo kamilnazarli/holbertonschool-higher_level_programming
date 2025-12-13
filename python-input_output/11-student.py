eefc847d2f5811c7120df7099ae8c2e9f100debc
Kamil <nezerovk425@gmail.com>
2025-12-13 18:38:28 +0000

    To create reload function

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module defines Student class
+"""
+
+
+class Student:
+    """
+    This class defines a student/
+    by their first, last name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            temp = {}
+            for i in attrs:
+                if self.__dict__.get(i) is not None:
+                    temp.update({i: self.__dict__.get(i)})
+            return temp
+
+    def reload_from_json(self, json):
+        for i in json:
+            if i in self.__dict__.keys():
+                self.__dict__[i] = json[i]
