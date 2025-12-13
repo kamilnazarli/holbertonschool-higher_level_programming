2745444364c18fbadbf87ea885b0baaf2d24a5d1
Kamil <nezerovk425@gmail.com>
2025-12-13 12:00:32 +0000

    To return JSON represantation

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,9 +2,9 @@
 """
 This module creates a function that returns json represantation of an object
 """
-
-
 import json
+
+
 def to_json_string(my_obj):
     """
     This function returns JSON represantation
