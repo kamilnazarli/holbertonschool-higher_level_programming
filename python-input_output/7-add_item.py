5de63b01342c4e81423841027acf15e9f276675a
Kamil <nezerovk425@gmail.com>
2025-12-13 12:23:53 +0000

    1

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,6 +3,8 @@
 This module adds all args to python list/
 and save them to the file
 """
+
+
 import json
 save_to_json_file = __import__(5-save_to_json_file).save_to_json_file
 load_from_json_file = __import__(6-load_from_json_file).load_from_json_file
