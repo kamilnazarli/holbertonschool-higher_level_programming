47fa585d127373353d02ef556a80e20a22ef7f7d
Kamil <nezerovk425@gmail.com>
2025-12-13 15:06:10 +0000

    Add all arguments to python list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,17 @@ and save them to the file
 """
 
 
-import json
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-temp = input().split()
-save_to_json_file(temp, add_item.json)
+from os import path
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+if path.exists('add_item.json'):
+    obj_json_file = load_from_json_file('add_item.json')
+else:
+    obj_json_file = []
+
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
+
+save_to_json_file(obj_json_file, 'add_item.json')
