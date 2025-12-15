61cc068233eff738085eb745623fd5137aecd2af
Kamil <nezerovk425@gmail.com>
2025-12-15 17:33:50 +0000

    To delete by value

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    a_dictionary = str(a_dictionary).replace("'", "'").replace('"', '"')
     temp = a_dictionary.copy()
     for k in a_dictionary.keys():
         if temp.get(k) == value:
