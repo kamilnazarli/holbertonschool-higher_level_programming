8ce9913cdef4813b317ba6d8b18f344c28a11c0c
Kamil <nezerovk425@gmail.com>
2025-12-16 08:21:04 +0000

    To serialize a class object

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module defines a class with/
+serialize and deserialize functions
+"""
+import pickle
+class CustomObject:
+    """
+    This class defines a ccustom object with/
+    name, age, is_student
+    """
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'w') as f:
+                pickle.dump(self, f)
+        except:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'r') as f:
+                res = pickle.load(f)
+        except:
+            return None
+        return res
