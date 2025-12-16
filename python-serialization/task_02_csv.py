44dcb88caaa5666af32a2de71ecde40e25b1989a
Kamil <nezerovk425@gmail.com>
2025-12-16 09:23:58 +0000

    To convert csv file to json

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,8 +2,12 @@
 import csv
 import json
 def convert_csv_to_json(csv_file):
-    with open(csv_file, mode='r', newline='', encoding='utf-8') as f:
-        data = list(csv.DictReader(f))
+    try:
+        with open(csv_file, mode='r', newline='', encoding='utf-8') as f:
+            data = list(csv.DictReader(f))
 
-    with open('data.json', mode='w', encoding='utf-8') as f:
-        json.dump(data, f)
+        with open('data.json', mode='w', encoding='utf-8') as f:
+            json.dump(data, f)
+            return True
+    except:
+        return False
