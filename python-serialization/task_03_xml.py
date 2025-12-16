d4f8202c8098088b46caaa147a2600b246def6c4
Kamil <nezerovk425@gmail.com>
2025-12-16 09:44:46 +0000

    To convert xml to dict and vice versa

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -2,7 +2,7 @@
 import xml.etree.ElementTree as ET
 
 def serialize_to_xml(dictionary, filename):
-    root = ET.Element(root_name)
+    root = ET.Element(filename)
 
     for key, value in dictionary.items():
         child = ET.SubElement(root, key)
