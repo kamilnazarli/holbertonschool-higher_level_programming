41bd4d78ef2267234d5dddc2c802369bd3ace511
Kamil <nezerovk425@gmail.com>
2025-12-20 20:05:30 +0000

    To create all cities

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- To list all the cities of California
 SELECT id, name FROM cities WHERE state_id = (
-	SELECT id FROM states WHERE name = 'California' ORDER BY id;)
+	SELECT id FROM states WHERE name = 'California' ORDER BY id);
