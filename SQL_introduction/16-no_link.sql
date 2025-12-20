925366fab962b2578a8f77cf323538f20141b9d7
Kamil <nezerovk425@gmail.com>
2025-12-20 09:02:42 +0000

    To list all records that the score <> NULL

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- To list all records that the score is not NULL
-SELECT score, name FROM second_table WHERE score IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
