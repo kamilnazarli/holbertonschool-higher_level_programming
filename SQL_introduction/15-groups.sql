ef22ce776da14ece822adf0e0e938eb11c3b5512
Kamil <nezerovk425@gmail.com>
2025-12-20 08:58:17 +0000

    To group scores by their counts

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- To group score by their counts
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
