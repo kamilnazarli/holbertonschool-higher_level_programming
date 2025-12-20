d4316158a1e675b344f828af3494dcb55bcc1914
Kamil <nezerovk425@gmail.com>
2025-12-20 18:58:01 +0000

    To create a table with default value for id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- To create a table with default and unique value
-CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 AND UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE DEFAULT 1, name VARCHAR(256));
