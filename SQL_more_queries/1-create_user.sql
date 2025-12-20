6be348766dbc55d2a65f2f393a821c24dc4a84e4
Kamil <nezerovk425@gmail.com>
2025-12-20 17:34:17 +0000

    To create a user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- To create a user
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'USER_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
