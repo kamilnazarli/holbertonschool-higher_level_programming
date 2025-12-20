ba599528d41ae7a239227b3ff81f3baa172ca467
Kamil <nezerovk425@gmail.com>
2025-12-20 08:19:25 +0000

    To create and add values to the table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- To create a table and multiple rows
-CREATE TABLE IF EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES
 (1, 'John', 10),
