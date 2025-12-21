61210e153cb436369604a7c98f904cafda00ff3e
Kamil <neserovk425@gmail.com>
2025-12-21 09:46:05 +0000

    To list all show and genres linked to them

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,5 +4,5 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
-ON tv_show_genres.show_id = tv_genres.id
+ON tv_show_genres.show_id = tv_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name;
