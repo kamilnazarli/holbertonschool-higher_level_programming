3822db85203eb3676f0362b0830b585a9017607f
Kamil <neserovk425@gmail.com>
2025-12-21 07:20:46 +0000

    TO list cities in a database

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- To list all cities contained in a database
 SELECT cities.id, cities.name, states.name
-FROM states
-INNER JOIN cities
+FROM cities
+INNER JOIN states
 ON cities.id = states.id
 ORDER BY cities.id;
