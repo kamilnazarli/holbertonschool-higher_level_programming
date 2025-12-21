12936cdb27dbdc8b9e63d1e69521276bb4e1fcce
Kamil <neserovk425@gmail.com>
2025-12-21 07:51:19 +0000

    TO list cities in a database

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- To list all shows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
