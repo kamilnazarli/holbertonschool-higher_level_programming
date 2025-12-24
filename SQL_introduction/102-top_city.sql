f44b4513644074da67ed6327f57bb163a4c90d77
Kamil <nezerovk425@gmail.com>
2025-12-25 00:21:35 +0400

    To show top 3 cities based on temperatures

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- To display top 3 cities based on temp across different months
-SELECT city, SUM(value)/COUNT(city) AS avg_temp FROM temperatures WHERE (month = 'July' OR month = 'August') GROUP BY city ORDER BY avg_temp DESC
+SELECT city, SUM(value)/COUNT(city) AS avg_temp FROM temperatures WHERE (month = 7 OR month = 8) GROUP BY city ORDER BY avg_temp DESC
 LIMIT 3;
